eca0df6d2ccb358c4c099ab606eed10e1637100f
OmThawaregitgub <othaware175@gmail.com>
2025-06-22 14:15:23 +0530

    Update prediction_helper.py

--- app/prediction_helper.py
+++ app/prediction_helper.py
@@ -4,7 +4,9 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 # Path to the saved model and its components
-MODEL_PATH = 'artifacts\model_data.joblib'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_PATH = os.path.join(BASE_DIR, 'artifacts', 'model_data.joblib')
+
 
 # Load the model and its components
 model_data = joblib.load(MODEL_PATH)
